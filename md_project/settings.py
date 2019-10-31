b61e32fc4689b1a4714ea1d6169138aa45de80ae
Nick <plus0bit@gmail.com>
2019-10-31 10:42:41 +0300

    New updates for Heroku deployment

--- md_project/settings.py
+++ md_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&=o!dfyfh40y3%-)2pn&%02=*f+a%oe_moyik@hf&rqjui*196'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
